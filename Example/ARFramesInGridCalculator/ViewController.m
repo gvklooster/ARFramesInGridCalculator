26e95db56f4479409820e1cb263605a3606fb170
Gijs van Klooster <gijs@appyroad.com>
2015-04-29 13:59:25 +0200

    add segmented control for example project

--- Example/ARFramesInGridCalculator/ViewController.m
+++ Example/ARFramesInGridCalculator/ViewController.m
@@ -13,6 +13,8 @@
 
 @property (nonatomic, strong) ARFramesInGridCalculator *framesInGridCalculator;
 
+@property (nonatomic, strong) UIView *gridContainerView;
+
 @end
 
 @implementation ViewController
@@ -24,26 +26,65 @@
 	CGSize cellSize = CGSizeMake(50.0, 30.0);
 	
 	self.framesInGridCalculator = [[ARFramesInGridCalculator alloc] initWithCellSize:cellSize rowCount:4 columnCount:4 andDirection:ARFramesInGridDirectionHorizontal];
-	self.framesInGridCalculator.initialOffset = CGPointMake(20.0, 100.0);
+//	self.framesInGridCalculator.initialOffset = CGPointMake(10.0, 10.0);
 	self.framesInGridCalculator.cellSpacing = CGSizeMake(10.0, 10.0);
+	[self.framesInGridCalculator setCellWidthToFitWidth:CGRectGetWidth(self.view.bounds) numberOfCells:4 horizontalMargin:10];
 	
-	[self updateGridView];
+	
+	self.gridContainerView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 200.0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds))];
+	self.gridContainerView.backgroundColor = [[UIColor yellowColor] colorWithAlphaComponent:0.3];
+	[self.view addSubview:self.gridContainerView];
+	
+	[self updateGridContainerView];
+	
+	[self addControls];
 }
 
-- (void)updateGridView
+- (void)updateGridContainerView
 {
-	for (UIView *view in [self.view subviews]) {
+	for (UIView *view in [self.gridContainerView subviews]) {
 		[view removeFromSuperview];
 	}
 	
-	for (int i = 0; i < 15; i ++)
+	for (int i = 0; i < 14; i ++)
 	{
 		UILabel *test = [[UILabel alloc] initWithFrame:[self.framesInGridCalculator frameForIndex:i]];
 		test.backgroundColor = [UIColor colorWithWhite:0.5 alpha:0.5];
 		test.text = [NSString stringWithFormat:@"%i", i];
 		test.textAlignment = NSTextAlignmentCenter;
-		[self.view addSubview:test];
+		[self.gridContainerView addSubview:test];
 	}
+}
+
+- (void)addControls
+{
+	UISegmentedControl *segControl = [[UISegmentedControl alloc] initWithItems:@[@"Horizontal", @"Vertical"]];
+	segControl.frame = CGRectMake(10.0, 40.0, CGRectGetWidth(self.view.bounds) - 20.0, 30.0);
+	[segControl addTarget:self action:@selector(segmentedTapped:) forControlEvents:UIControlEventValueChanged];
+	segControl.selectedSegmentIndex = 0;
+	[self.view addSubview:segControl];
+}
+
+- (void)segmentedTapped:(UISegmentedControl *)sender
+{
+	switch (sender.selectedSegmentIndex)
+	{
+		case 0:
+		{
+			self.framesInGridCalculator.gridDirection = ARFramesInGridDirectionHorizontal;
+			break;
+		}
+		case 1:
+		{
+			self.framesInGridCalculator.gridDirection = ARFramesInGridDirectionVertical;
+			break;
+		}
+			
+		default:
+			break;
+	}
+	
+	[self updateGridContainerView];
 }
 
 @end
